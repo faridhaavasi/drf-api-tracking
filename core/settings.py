111927d186876b6e243c7b0dd664af1c014ac47d
faridhaavasi <faridhavasi777@gmail.com>
2024-06-18 20:40:26 +0330

    add rest framework

--- core/settings.py
+++ core/settings.py
@@ -31,12 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # core app
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd party app
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
